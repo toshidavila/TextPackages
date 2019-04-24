b2df19b951f818978f0c023310c616f51ac8b85a
toshi davila <toshi@assembla.com>
2019-04-23 20:31:53 -0500

    improving class

--- src/TransformUppercase.php
+++ src/TransformUppercase.php
@@ -5,17 +5,8 @@ namespace PackageToshi;
  */
 class PackageToshi
 {
-    function __construct($text)
-    {
-        $this->setUpperText($text);
-    }
-    private function getUpperText($text)
-    {
-        return $text;
-    }
     public function setUpperText($text)
     {
-        $newText = $this->getUpperText($text);
         return strtoupper($newText);
     }
 }
